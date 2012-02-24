42063020f4974f81e15db64ff7dc123c8621e740
serge-at-cobalys <serge@cobalys.com>
2012-02-24 04:34:34 -0200

    Settings template

--- reporteaccidentes/urls.py
+++ reporteaccidentes/urls.py
@@ -2,26 +2,11 @@ from django.conf.urls.defaults import *
 from reporteaccidentes.accidentes.views import index, get_top, about, top
 from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Example:
-    # (r'^reporteaccidentes/', include('reporteaccidentes.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
      (r'^$', index),
      (r'^update.ajax', get_top),
      (r'^ranking.html', top),
      (r'^about.html', about),
 )
-
-
 if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_DOC_ROOT}),
-    )
+    urlpatterns += staticfiles_urlpatterns()
